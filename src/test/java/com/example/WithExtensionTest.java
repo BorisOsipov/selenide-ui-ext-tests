b0d9cb31a660ec65d31d95c6f6d3a0813fafaf40
Boris Osipov <osipov.boris@gmail.com>
2020-11-12 14:49:28 +0300

    Add samples

--- src/test/java/com/example/WithExtensionTest.java
+++ src/test/java/com/example/WithExtensionTest.java
@@ -1,13 +1,16 @@
 package com.example;
 
+import com.codeborne.selenide.Browser;
+import com.codeborne.selenide.Config;
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.WebDriverRunner;
+import com.codeborne.selenide.webdriver.ChromeDriverFactory;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.MutableCapabilities;
+import org.openqa.selenium.Proxy;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.firefox.FirefoxOptions;
-import org.openqa.selenium.firefox.FirefoxProfile;
 
 import java.io.File;
 
@@ -16,52 +19,37 @@ import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
-/**
- * Simple Selenide Test with PageObjects
- */
 class WithExtensionTest {
-
     @Test
     void openWithChromeExtensionBySelenide() {
-        String extPath = this.getClass().getClassLoader().getResource("chrome_cryptopro_ext.crx").getFile();
-        ChromeOptions options = new ChromeOptions();
-        options.addExtensions(new File(extPath));
-
+        Configuration.browserCapabilities = getChromeOptionsWithExtension();
         Configuration.browser = "chrome";
-        Configuration.browserCapabilities = options;
 
         open("https://www.cryptopro.ru/sites/default/files/products/cades/demopage/main.html");
         $(byText("ЭЦП в браузере – попробуйте прямо сейчас!")).should(visible);
-
     }
 
     @Test
-    void openWithChromeExtensionByChromeDriver() {
+    void openWithChromeExtensionByRemote() {
+        Configuration.browserCapabilities = getChromeOptionsWithExtension();
+        Configuration.browser = CustomChromeDriverFactory.class.getName();
+        Configuration.remote = "http://localhost:4444/wd/hub";
+
+        open("https://www.cryptopro.ru/sites/default/files/products/cades/demopage/main.html");
+        $(byText("ЭЦП в браузере – попробуйте прямо сейчас!")).should(visible);
+    }
+
+    private ChromeOptions getChromeOptionsWithExtension() {
         String extPath = this.getClass().getClassLoader().getResource("chrome_cryptopro_ext.crx").getFile();
         ChromeOptions options = new ChromeOptions();
         options.addExtensions(new File(extPath));
-
-        WebDriverManager.chromedriver().setup();
-        ChromeDriver driver = new ChromeDriver(options);
-        WebDriverRunner.setWebDriver(driver);
-
-        open("https://www.cryptopro.ru/sites/default/files/products/cades/demopage/main.html");
-        $(byText("ЭЦП в браузере – попробуйте прямо сейчас!")).should(visible);
-        WebDriverRunner.closeWebDriver();
+        return options;
     }
+}
 
-    @Test
-    void openWithFirefoxExtensionBySelenide() {
-        FirefoxOptions options = new FirefoxOptions();
-        String extPath = this.getClass().getClassLoader().getResource("ff_cryptopro_ext.xpi").getFile();
-        FirefoxProfile profile = new FirefoxProfile();
-        profile.addExtension(new File(extPath));
-        options.setProfile(profile);
-
-        Configuration.browser = "firefox";
-        Configuration.browserCapabilities = options;
-
-        open("https://www.cryptopro.ru/sites/default/files/products/cades/demopage/main.html");
-        $(byText("ЭЦП в браузере – попробуйте прямо сейчас!")).should(visible);
+class CustomChromeDriverFactory extends ChromeDriverFactory {
+    @Override
+    protected String[] excludeSwitches() {
+        return new String[]{"enable-automation"};
     }
 }
